f7de0b5417d11926e8b6b9ca44ed32046d64f0be
Nguyễn Đắc Hiếu <ndhhieuu130304@gmail.com>
2024-12-03 18:58:52 +0700

    ndh

--- BTVN/views/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="separator"></div>
-<footer>
-    <?php
-    $footerContent = "TLU'S MUSIC GARDEN";
-    echo "<p>" . htmlspecialchars($footerContent, ENT_QUOTES, 'UTF-8') . "</p>";
-    ?>
-</footer>
