c65e83fc5c558b105ae7dd0392e157bd1b51f9d9
Nguyễn Đắc Hiếu <ndhhieuu130304@gmail.com>
2024-12-12 09:24:55 +0700

    btth3

--- lrv_wedbanhang/database/seeders/DatabaseSeeder.php
+++ lrv_wedbanhang/database/seeders/DatabaseSeeder.php
@@ -2,17 +2,19 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use PharIo\Manifest\Library;
 
 class DatabaseSeeder extends Seeder
 {
-    public function run()
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
     {
-        // Gọi các Seeder để chèn dữ liệu
-        $this->call([
-            LibrariesTableSeeder::class,
-            BooksTableSeeder::class,
-        ]);
+        // User::factory(10)->create();
+
+       
     }
 }
